bc70c4e953fdc80792651234572714e57dc74fc9
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-24 17:58:49 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -31,7 +31,7 @@ int print_ident(char letter, ...)
 int _printf(const char *format, ...)
 {
 	unsigned int i;
-	int identifierPrinted = 0, charPrinted = 0;
+	int identifier = 0, charPrinted = 0;
 	va_list args;
 	
 	if (format == NULL)
@@ -53,12 +53,12 @@ int _printf(const char *format, ...)
 		}
 		if (format[i + 1] == '\0')
 			return (-1);
-		identifierPrinted = printident(format[i + 1], arg);
-		if (identifierPrinted == -1 || identifierPrinted != 0)
+		identifier = print_ident(format[i + 1], arg);
+		if (identifier == -1 || identifier != 0)
 			i++;
-		if (identifierPrinted > 0)
-			charPrinted += identifierPrinted;
-		if (identifierPrinted == 0)
+		if (identifier > 0)
+			charPrinted += identifier;
+		if (identifier == 0)
 		{
 			_putchar('%');
 			charPrinted++;
