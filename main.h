bc4af0ca2a25f91f5e1fbb6a07398ec8d0fa9e2e
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-24 15:00:26 +0300

    Header file

--- main.h
+++ main.h
@@ -1,6 +1,27 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <stdarg.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_int(va_list arg);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+
+
+
+/**
+  * struct ident - struct
+  *
+  *
+  */
+typedef struct ident
+{
+	char *type;
+	int (*f)(va_list);
+};
+
+
 
 #endif /* _MAIN_H */
