bb79fa9e06a2a7cc45bd4d1974f0140b4a6943f4
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-25 13:07:34 +0300

    Complete

--- _printf.c
+++ _printf.c
@@ -17,6 +17,14 @@ int printIdentifiers(char next, va_list arg)
 	identifierStruct functs[] = {
 		{"c", print_char},
 		{"s", print_str},
+		{"d", print_int},
+		{"i", print_int},
+		{"u", print_unsigned},
+		{"b", print_unsignedToBinary},
+		{"o", print_oct},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"S", print_STR},
 		{NULL, NULL}
 	};
 
